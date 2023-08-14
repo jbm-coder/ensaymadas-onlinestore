fe2cdb5fcf5089a8b62d822b18330cee76c6586b
jbm-coder <115842129+jbm-coder@users.noreply.github.com>
2023-08-14 14:34:57 +1000

    setup header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
 
 const App = () => {
-  return <h1>Welcome to ensaymadas</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ensaymadas</h1>
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default App;
