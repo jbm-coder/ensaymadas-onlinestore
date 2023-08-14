35cdaa40f17294c9952e7070d99352e1bbbcfc40
jbm-coder <115842129+jbm-coder@users.noreply.github.com>
2023-08-14 22:35:27 +1000

    update product details

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -3,7 +3,8 @@ const products = [
     _id: '1',
     name: 'Ensaymadas',
     image: '/images/original-ensaymadas.jpg',
-    description: 'Most Popular - Original Ensaymadas',
+    description:
+      'Indulge in the ultimate comfort treat with our moist and tasty original ensaymadas. These buttery, soft pastries are a delightful blend of tradition and flavor. Baked to perfection, they offer a moment of pure joy with each bite.',
     price: 3.5,
     rating: 4.9,
     numReviews: 12,
@@ -12,7 +13,8 @@ const products = [
     _id: '2',
     name: 'Ube Ensaymadas',
     image: '/images/ube-ensaymadas.jpeg',
-    description: 'Ube Ensaymadas',
+    description:
+      'Experience a burst of flavor with our Ube Ensaymadas. These pastries beautifully blend tradition with innovation, featuring the rich taste of purple yam. Each bite delivers a moist, tender, and captivating experience. Indulge in the perfect balance of elegance and comfort.',
     price: 4.5,
     rating: 4.9,
     numReviews: 12,
@@ -21,7 +23,8 @@ const products = [
     _id: '3',
     name: 'Ube Halaya',
     image: '/images/ube-halaya.jpg',
-    description: 'Ube Halaya',
+    description:
+      'Experience the allure of Ube Halaya, reimagined with a delightful twist. Our Ube Halaya captures the essence of purple yam in every spoonful. Enjoy the velvety texture and vibrant flavor of this Filipino favorite.',
     price: 8.5,
     rating: 4.9,
     numReviews: 12,
@@ -30,7 +33,8 @@ const products = [
     _id: '4',
     name: 'Cassava Cake',
     image: '/images/cassava.jpg',
-    description: 'Cassava Cake',
+    description:
+      'Discover the essence of satisfaction in every bite with our Moist Cassava Cake. Popular and perfected, this Filipino delight boasts a deliciously tender texture and a delectable crust topping.',
     price: 12.5,
     rating: 4.9,
     numReviews: 12,
