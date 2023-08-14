bb6ba6c87f59025509b2038f97de1cb2f82bdbb7
jbm-coder <115842129+jbm-coder@users.noreply.github.com>
2023-08-14 16:25:00 +1000

    implement rating system

---
+++ frontend/src/products.js
@@ -0,0 +1,40 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Ensaymadas',
+    image: './images/original-ensaymadas.jpg',
+    description: 'Most Popular - Original Ensaymadas',
+    price: 3.5,
+    rating: 4.9,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Ube Ensaymadas',
+    image: './images/ube-ensaymadas.jpeg',
+    description: 'Ube Ensaymadas',
+    price: 4.5,
+    rating: 4.9,
+    numReviews: 12,
+  },
+  {
+    _id: '3',
+    name: 'Ube Halaya',
+    image: './images/ube-halaya.jpg',
+    description: 'Ube Halaya',
+    price: 8.5,
+    rating: 4.9,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Cassava Cake',
+    image: './images/cassava.jpg',
+    description: 'Cassava Cake',
+    price: 12.5,
+    rating: 4.9,
+    numReviews: 12,
+  },
+];
+
+export default products;
